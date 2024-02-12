76ac9081c5d6443041562c2dfa42fdb24553335c
Hever2000 <santicoronellancellotti@gmail.com>
2024-02-12 17:49:39 -0300

    nuevo commit js

---
+++ app.js
@@ -0,0 +1,12 @@
+
+const typed = new Typed(".typed", {
+  strings : ["Santiago", "Coronel"],
+  stringsElement: ".typed-element",
+  typeSpeed: 75,
+  backSpeed: 75,
+  backDelay: 1000,
+  startDelay: 300,
+  loop: true,
+  cursorChar: "|"
+});
+  
