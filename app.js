9f6b47291f5d1d891ebe76b692696419b99783eb
Hever2000 <santicoronellancellotti@gmail.com>
2024-02-23 03:25:58 -0300

    otro commit

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-
-const typed = new Typed(".typed", {
-  strings : ["Santiago", "Coronel"],
-  stringsElement: ".typed-element",
-  typeSpeed: 75,
-  backSpeed: 75,
-  backDelay: 1000,
-  startDelay: 300,
-  loop: true,
-  cursorChar: "|"
-});
-  
+//const typed = new Typed(".typed", {
+//strings : ["Santiago", "Coronel"],
+//stringsElement: ".typed-element",
+// typeSpeed: 75,
+// backSpeed: 75,
+// backDelay: 1000,
+//startDelay: 300,
+// loop: true,
+// cursorChar: "|"
+//});
