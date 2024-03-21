424e7afa22f840f5b7b041d1bea3c507ef859ef4
Hever2000 <santicoronellancellotti@gmail.com>
2024-03-21 15:20:53 -0300

    js y cambios con flexbox (responsive)

--- app.js
+++ app.js
@@ -9,3 +9,17 @@
 // cursorChar: "|"
 //});
 
+//const navLinksList = document.querySelectorAll(".inicio a");
+
+//navLinksList.forEach((link) => {
+//    const targetElement = document.getElementById(targetId);
+//    if (targetElement) {
+//      const offsetTop =
+//        targetElement.getBoundingClientRect().top + window.pageYOffset;
+//      window.scrollTo({
+//        top: offsetTop,
+//        behavior: "smooth",
+//      });
+//    }
+//  });
+//});
